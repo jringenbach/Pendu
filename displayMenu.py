78b038186714347a188a0ae3eb9ce6ddbb555834
jringenbach <ringenbach.julien@gmail.com>
2018-10-11 10:47:52 +0200

    Player can chose his pseudonyme and the difficulty

--- displayMenu.py
+++ displayMenu.py
@@ -3,9 +3,10 @@
 class DisplayMenu:
     """ class containing the method to display different menus"""
     
-    def __init__(self, options):
+    def __init__(self, options, askingOption):
         self.options = options
         self.nbOptions = len(options)
+        self.askingOption = askingOption
 
     def display(self):
         """ Display a menu with the different options offered to the player"""
@@ -13,3 +14,6 @@ class DisplayMenu:
         for option in self.options:
             print(str(i)+ ". "+self.options[i-1])
             i = i + 1
+
+        #Returning the choice made by the player
+        return input(self.askingOption)
--- main.py
+++ main.py
@@ -6,3 +6,15 @@ from game import Game
 from displayMenu import DisplayMenu
 
 """ Main file containing the game """
+keepPlaying = True
+
+while keepPlaying == True:
+    #The player chose his/her pseudonyme
+    pseudoPlayer = input("Choisissez un pseudonyme : ")
+    player = Player(pseudoPlayer)
+
+    #The player chose the difficulty of the game
+    difficultyList = ["Facile", "Normal", "Difficile"]
+    difficultyAsking = "Choisissez votre niveau de difficulté : "
+    difficultyMenu = DisplayMenu(difficultyList, difficultyAsking)
+    difficultyMenu.display()
